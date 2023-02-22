3400457996fae15ab2dab08607b5d72147fc1ca9
NadiaJoy <iminneverwhere@gmail.com>
2023-02-22 22:07:42 +0200

    Part1

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,31 +1,77 @@
 package org.example;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-
-//    @Test
-// public void shouldAnswerWithTrue()
-// {
-//     assertTrue( true );
-// }
+public class AppTest {
+    //ex.4
 
     @Test
-    public void checkSumming()
-    {
+    public void checkSumPositiveNumbers() {
         //Arrange
         Calculator calc = new Calculator();
         //Act
         int result = calc.sumOfTwo(3, 5);
         //Assert
         assertEquals(8, result);
+
+    }
+
+    @Test
+    public void checkSumNegativeNumbers() {
+        //Arrange
+        Calculator calc = new Calculator();
+        //Act
+        int result = calc.sumOfTwo(-8, -2);
+        //Assert
+        assertEquals(-10, result);
+
+    }
+
+    @Test
+    public void checkSubtractionPositiveNumbers() {
+        //Arrange
+        Calculator calc = new Calculator();
+        //Act
+        int result = calc.diffOfTwo(10, 1);
+        //Assert
+        assertEquals(9, result);
+
+    }
+
+    @Test
+    public void checkSubtractionNegativeNumbers() {
+        //Arrange
+        Calculator calc = new Calculator();
+        //Act
+        int result = calc.diffOfTwo(-1, -1);
+        //Assert
+        //!!!!!!!!!!!!! math
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void checkSubtractionDiffNumbersNegPos() {
+        //Arrange
+        Calculator calc = new Calculator();
+        //Act
+        int result = calc.diffOfTwo(-1, 14);
+        //Assert
+        assertEquals(-15, result);
+
+    }
+    @Test
+    public void checkSubtractionDiffNumbersPosNeg() {
+        //Arrange
+        Calculator calc = new Calculator();
+        //Act
+        int result = calc.diffOfTwo(1, -1);
+        //Assert
+        assertEquals(2, result);
 
     }
 }
